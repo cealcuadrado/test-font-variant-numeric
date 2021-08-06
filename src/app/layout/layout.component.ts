80cb1b48eb8c6971de04fb425e230d2c1ad97756
CeAlCuadrado <cealcuadrado@outlook.com>
2021-08-06 16:19:33 -0400

    Definido layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  fontValue: string = '';
 
-  constructor() { }
+  fontValues = [
+    {
+      category: 'Normal',
+      values: ['normal'],
+    },
+    {
+      category: 'Special Glyphs',
+      values: ['ordinal'],
+    },
+    {
+      category: 'Zero with slash',
+      values: ['slashed-zero'],
+    },
+    {
+      category: 'Numeric Figure Values',
+      values: ['lining-nums', 'oldstyle-nums'],
+    },
+    {
+      category: 'Numeric Spacing Values',
+      values: ['proportional-nums', 'tabular-nums']
+    },
+    {
+      category: 'Numeric Fraction values',
+      values: ['diagonal-fractions', 'stacked-fractions'],
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  rows = [
+    { hours: '07:00 - 08:00', columnOne: '1', columnTwo: '2' },
+    { hours: '08:00 - 09:00', columnOne: '1', columnTwo: '2' },
+    { hours: '09:00 - 10:00', columnOne: '1', columnTwo: '2' },
+    { hours: '10:00 - 11:00', columnOne: '1', columnTwo: '2' },
+    { hours: '11:00 - 12:00', columnOne: '1', columnTwo: '2' },
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
